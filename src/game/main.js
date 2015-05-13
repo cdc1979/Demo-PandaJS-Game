2659af1974efbe55fa7bf245fce2a0da9d2a8ee1
chris.d.casey@googlemail.com <chris.d.casey@googlemail.com>
2015-05-13 16:20:38 +0100

    Added sprites

--- src/game/main.js
+++ src/game/main.js
@@ -3,25 +3,86 @@ game.module(
 )
 .body(function() {
 
-game.addAsset('panda.png');
+    //game.addAsset('panda.png');
+    game.addAsset('Background.jpg');
+    game.addAsset('Circle.png');
+    game.addAsset('Square.png');
+    game.addAsset('Triangle.png');
+    game.addAsset('Player.png');
+
+
+game.createClass('Block', {
+        init: function (x,y,spritename) {
+            //this._super(spritename, x, y, { anchor: { x: 0.5, y: 0.5 } });
+            this.sprite = new game.Sprite(spritename);
+            this.sprite.position = { x: x, y: y };
+            //game.scene.addObject(this);
+            //add body of this sprite to the world object
+            //game.scene.world.addBody(this.body);
+            //add sprite to display container
+            //game.scene.stage.addChild(this);
+            game.scene.stage.addChild(this.sprite);
+        }
+});
+
+
+game.createClass('Ball', {
+    interactive: true,
+    init: function (x, y) {
+        //this._super(spritename, x, y, { anchor: { x: 0.5, y: 0.5 } });
+        this.sprite = new game.Sprite('Player.png');
+        this.sprite.position = { x: x, y: y };
+        
+        //add body of this sprite to the world object
+        //game.scene.world.addBody(this.body);
+        //add sprite to display container
+        //game.scene.stage.addChild(this);
+        game.scene.addObject(this.sprite);
+        game.scene.stage.addChild(this.sprite);
+    },
+    update: function () {
+        console.log("test");
+            // Check if key is currently down
+        if (game.keyboard.down('SPACE')) {
+           
+        }
+
+    },
+});
 
 game.createScene('Main', {
     backgroundColor: 0xb9bec7,
+
     init: function() {
-        
-        //create emitter
-        var emitter = new game.Emitter();
-        //set visualDisplayContainer were the images will be emitted.
-        //(Usually this is game.scene.stage but remember that you could also emit particles from within the container of an existing sprite.)
-        emitter.container = this.stage;
-        emitter.angleVar = 90;
-        emitter.life = 10000;
-        //Set texture to emit
-        emitter.textures.push('panda.png');
-        //set position
-        emitter.position.set(game.system.width / 2, game.system.height / 2);
-        //add emitter to the scene so it will start working.
-        //this.addEmitter(emitter); // */
+
+        var sprite = new game.TilingSprite('Background.jpg', 0, 0);
+        sprite.addTo(game.scene.stage);
+
+        for (var i = 0; i < 5; i++) {
+            var rnd = (Math.random() * game.system.width);
+            if (rnd > 850) 
+            {
+                rnd = 850;
+            }
+            test = new game.Block(rnd, Math.random() * game.system.height, 'Circle.png');
+        }
+        for (var i = 0; i < 5; i++) {
+            var rnd = (Math.random() * game.system.width);
+            if (rnd > 800) {
+                rnd = 800;
+            }
+            test = new game.Block(rnd, Math.random() * game.system.height, 'Triangle.png');
+        }
+        for (var i = 0; i < 5; i++) {
+            var rnd = (Math.random() * game.system.width);
+            if (rnd > 800) {
+                rnd = 800;
+            }
+            test = new game.Block(rnd, Math.random() * game.system.height, 'Square.png');
+        }
+
+        new game.Ball(920, 350, 'Player.png');
+
 
     }
 });
